6a7a01175eb5f2822cf9549ef553ed48ca7a099f
thegreatsantini <lss13@my.fsu.edu>
2018-10-06 21:18:12 -0700

    completed functionality for chekcout chapter

--- TDD_Node/checkout.js
+++ TDD_Node/checkout.js
@@ -1,12 +1,57 @@
 module.exports = class Checkout {
-    addItemPrice() {
-
+    constructor() {
+        this.prices = {},
+            this.items = {},
+            this.discounts = {}
+    }
+    addItemPrice(item, price) {
+        this.prices[item] = price
     }
 
-    addItem() {
-
+    addItem(item) {
+        if ( this.prices[item] ===undefined ) {
+            throw("No price defined for item")
+        }
+        if (this.items[item] === undefined) {
+            this.items[item] = 1;
+        } else {
+            this.items[item]++;
+        }
     }
     calculateTotal() {
-        return 1;
+        let total = 0;
+
+        for (let item in this.items) {
+            total += this.calculateItemTotal(item)
+        }
+        return total;
+    }
+    calculateItemTotal(item) {
+        let total = 0;
+        const discount = this.discounts[item];
+        if (discount != undefined) {
+            total += this.calculateDiscount(item, this.items[item], discount);
+        }
+        // There are no discounts to apply 
+        else {
+            total += (this.prices[item] * this.items[item]);
+        }
+        return total
+    }
+    calculateDiscount(item, itemCnt, discount) {
+        let total = 0;
+        const nbrOfDiscounts = itemCnt / discount.cnt;
+        total += nbrOfDiscounts * discount.price;
+        // Calculate number of items that don't get discount
+        const remainder = itemCnt % discount.cnt;
+        total += remainder * this.prices[item];
+        return total;
+    }
+
+    addDiscount(item, itemCnt, discountPrice) {
+        this.discounts[item] = {
+            cnt: itemCnt,
+            price: discountPrice
+        }
     }
 }
